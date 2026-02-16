d3fc902449efaa9d5347e6a6d95d8a723f3cf28f
feed0 <dogmas-gutters.07@icloud.com>
2026-02-16 18:40:23 -0300

    [BD-01] Create a type to represent a friend 1. ADDED: Friend

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Felipe Eduardo Campelo Ferreira Osorio on 16/02/26.
+//
+
+import Foundation
+
+struct Friend {
+    var name: String
+    var birthday: Date
+}
