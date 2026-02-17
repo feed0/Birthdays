b4edf33163176421f3bf64520f087b6eff6659f4
feed0 <dogmas-gutters.07@icloud.com>
2026-02-17 09:36:23 -0300

    [BD-02] Create a type to represent a friend 1. ADDED: New birthday safe area inset

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Properties
+    
     @State private var friends: [Friend] = [
         Friend(name: "Pedro", birthday: .now),
         Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0))
     ]
+    
+    @State private var newName = ""
+    @State private var newDate = Date.now
+    
+    // MARK: - Body
     
     var body: some View {
         NavigationStack {
@@ -24,6 +32,28 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newDate,
+                               in: Date.distantPast...Date.now,
+                               displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newDate)
+                        friends.append(newFriend)
+                        
+                        newName = ""
+                        newDate = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
         }
     }
 }
